f9380afa82d1ecf1b546b070ee0eca7998ebe4c9
Jatawatsafe <35445358+safesit23@users.noreply.github.com>
2024-05-09 20:23:28 +0700

    Update and rename mycode.py to challenge-1.py

---
+++ challenge-1.py
@@ -0,0 +1,2 @@
+def sum_of_multiples(n: int, x: int) -> int:
+    pass
--- mycode.py
+++
@@ -1,6 +0,0 @@
-number = 7
-
-if number > 10 :
-  print("เงื่อนไขที่ 1 : number > 10")
-else :
-  print("เงื่อนไขอื่นๆ : number < 10")
